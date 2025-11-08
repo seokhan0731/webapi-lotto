a319fd94e681b36ac411ba3823246d255055a278
seokhan0731 <wistin@naver.com>
2025-11-08 18:19:51 +0900

    build: 프로젝트 개설 및 README.md 추가

---
+++ src/main/java/precourse/openmission/OpenmissionApplication.java
@@ -0,0 +1,13 @@
+package precourse.openmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/precourse/openmission/OpenmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package precourse.openmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
