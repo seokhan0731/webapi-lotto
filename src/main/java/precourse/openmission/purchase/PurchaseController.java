c8313f5c97fd59a72b968e8a606f8e16874a3623
seokhan0731 <wistin@naver.com>
2025-11-10 01:18:28 +0900

    feat: 천원으로 안 나누어 떨어지는 구매 금액 요청 받은 경우, 상태코드 400과 에러 메시지 반환

--- src/main/java/precourse/openmission/purchase/PurchaseController.java
+++ src/main/java/precourse/openmission/purchase/PurchaseController.java
@@ -1,8 +1,11 @@
 package precourse.openmission.purchase;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,10 @@ public class PurchaseController {
     @PostMapping(value = "/purchase", produces = MediaType.APPLICATION_JSON_VALUE)
     public Purchase savePurchase(@RequestBody PurchaseRequestDTO purchaseRequestDTO) {
         return purchaseService.purchase(purchaseRequestDTO.getMoney());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
