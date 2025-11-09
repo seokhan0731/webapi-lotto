a3a40316b7b72daba9e42e8505ef8f6bf2533abb
seokhan0731 <wistin@naver.com>
2025-11-09 23:00:20 +0900

    test: PurchaseService 계층 내 Amount가 발생시키는 에러 메시지 인식 테스트

--- src/test/java/precourse/openmission/purchase/PurchaseServiceTest.java
+++ src/test/java/precourse/openmission/purchase/PurchaseServiceTest.java
@@ -50,4 +50,27 @@ public class PurchaseServiceTest {
         assertThat(getByService.get(0).getAmount()).isEqualTo(2000);
         assertThat(getByService.get(0).getQuantity()).isEqualTo(2);
     }
+
+    @DisplayName("구입 금액 1,000원으로 나누어 떨어지지 않는 경우 예외처리")
+    @Test
+    void amountDivideException() {
+        //Given
+        int money = 1500;
+
+        //When, Then
+        assertThatThrownBy(() -> purchaseService.purchase(money)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1,000원으로 나누어 떨어져야합니다.");
+    }
+
+    @DisplayName("구입 금액 양수가 아닌 경우 예외처리")
+    @Test
+    void notPositiveAmount() {
+        //Given
+        int money = -1000;
+
+        //When, Then
+        assertThatThrownBy(() -> purchaseService.purchase(money)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 양수여야합니다.");
+    }
+
 }
