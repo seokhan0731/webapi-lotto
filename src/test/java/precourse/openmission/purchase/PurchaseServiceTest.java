f9441faf2bac20a996130d1bb0599a9bfbf695be
seokhan0731 <wistin@naver.com>
2025-11-09 22:25:38 +0900

    test: PurchaseService를 통한 구매 금액 및 수량 저장 테스트

---
+++ src/test/java/precourse/openmission/purchase/PurchaseServiceTest.java
@@ -0,0 +1,35 @@
+package precourse.openmission.purchase;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PurchaseServiceTest {
+    @Autowired
+    PurchaseService purchaseService;
+
+    @Autowired
+    PurchaseRepository purchaseRepository;
+
+    @DisplayName("db에 구매 금액 및 수량 저장 지휘")
+    @Test
+    void saveByService() {
+        //Given
+        int money = 2000;
+
+        //When
+        purchaseService.purchase(money);
+
+        //Then
+        List<Purchase> found = purchaseRepository.findAll();
+        assertThat(found).hasSize(1);
+        assertThat(found.get(0).getAmount()).isEqualTo(money);
+        assertThat(found.get(0).getQuantity()).isEqualTo(2);
+    }
+}
