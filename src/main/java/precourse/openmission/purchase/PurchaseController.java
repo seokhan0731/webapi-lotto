acd39859ad1fdae12e8a25467982cf8904e2e737
seokhan0731 <wistin@naver.com>
2025-11-10 01:46:30 +0900

    feat: 구매내역 조회 GET 요청 시 JSON으로 반환 구현

--- src/main/java/precourse/openmission/purchase/PurchaseController.java
+++ src/main/java/precourse/openmission/purchase/PurchaseController.java
@@ -4,13 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +22,10 @@ public class PurchaseController {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @GetMapping(value = "/purchase/history", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Purchase> getHistory() {
+        return purchaseService.getPurchases();
     }
 }
