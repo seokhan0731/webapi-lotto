9b861664cdf1cf79cb06d6e0fa3b5b931b363dcb
seokhan0731 <wistin@naver.com>
2025-11-10 21:53:27 +0900

    feat: 서비스 계층에서 발행한 로또 번호들 db에 저장

---
+++ src/main/java/precourse/openmission/issue/MyLottoService.java
@@ -0,0 +1,55 @@
+package precourse.openmission.issue;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import precourse.openmission.domain.Lottos;
+import precourse.openmission.domain.NumberGenerator;
+import precourse.openmission.purchase.Purchase;
+import precourse.openmission.purchase.PurchaseRepository;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MyLottoService {
+
+    private final MyLottoRepository lottoRepository;
+    private final PurchaseRepository purchaseRepository;
+    private final NumberGenerator numberGenerator;
+
+    public List<MyLotto> saveLottos(Long purchaseId) {
+        Purchase foundPurchase = validateId(purchaseId);
+        List<MyLotto> lottosToSave = createListOfLottoEntities(foundPurchase);
+        return lottoRepository.saveAll(lottosToSave);
+    }
+
+    private Purchase validateId(Long purchaseId) {
+        return purchaseRepository.findById(purchaseId).orElseThrow(() ->
+                new IllegalArgumentException("[ERROR] 해당 구매 내역을 찾을 수 없습니다."));
+    }
+
+    private List<Set<Integer>> issueLottos(Purchase foundPurchase) {
+        Lottos myLottos = new Lottos(foundPurchase.getQuantity());
+        myLottos.issueLottos(numberGenerator);
+        return myLottos.getNumbersAll();
+    }
+
+    private String setToString(Set<Integer> set) {
+        return set.stream()
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+    }
+
+    private List<MyLotto> createListOfLottoEntities(Purchase foundPurchase) {
+        List<Set<Integer>> allNumbersSet = issueLottos(foundPurchase);
+        return allNumbersSet.stream()
+                .map(singleSet -> {
+                    String numbersAsString = setToString(singleSet);
+                    return new MyLotto(numbersAsString, foundPurchase);
+                })
+                .toList();
+    }
+}
