8414cf0a48fedb3c5b23aaee7adb79a849209ddc
seokhan0731 <wistin@naver.com>
2025-11-10 22:40:51 +0900

    test: 서비스 계층에서 발행한 로또 번호 조회 테스트

--- src/test/java/precourse/openmission/issue/MyLottoServiceTest.java
+++ src/test/java/precourse/openmission/issue/MyLottoServiceTest.java
@@ -39,23 +39,26 @@ public class MyLottoServiceTest {
     @Autowired
     NumberGenerator numberGenerator;
 
+
+    Purchase savedPurchase;
+    Long purchaseId;
+
     @BeforeEach
     void setUp() {
         myLottoRepository.deleteAll();
         purchaseRepository.deleteAll();
+
+        Purchase purchase = new Purchase(2000, 2);
+        savedPurchase = purchaseRepository.save(purchase);
+        purchaseId = savedPurchase.getId();
+        Mockito.when(numberGenerator.generateNumbers())
+                .thenReturn(List.of(1, 2, 3, 4, 5, 6))
+                .thenReturn(List.of(7, 8, 9, 10, 11, 12));
     }
 
     @DisplayName("db에 해당 수량에 따른 로또 번호 저장 지휘")
     @Test
     void saveMyLottos() {
-        //Given
-        Purchase purchase = new Purchase(2000, 2);
-        Purchase savedPurchase = purchaseRepository.save(purchase);
-        Long purchaseId = savedPurchase.getId();
-        Mockito.when(numberGenerator.generateNumbers())
-                .thenReturn(List.of(1, 2, 3, 4, 5, 6))
-                .thenReturn(List.of(7, 8, 9, 10, 11, 12));
-
         //When
         List<MyLotto> returnLottos = myLottoService.saveLottos(purchaseId);
 
@@ -69,4 +72,20 @@ public class MyLottoServiceTest {
         );
     }
 
+    @DisplayName("ID에 따른 발행된 로또 번호 조회")
+    @Test
+    void getMyLottos() {
+        //When
+        List<MyLotto> Lottos = myLottoService.saveLottos(purchaseId);
+        List<MyLotto> returnLottos = myLottoService.getLottos(purchaseId);
+
+        //Then
+        assertAll(
+                () -> assertThat(returnLottos).hasSize(2),
+                () -> assertThat(returnLottos.get(0).getPurchase().getId()).isEqualTo(purchaseId),
+                () -> assertThat(returnLottos.get(1).getPurchase().getId()).isEqualTo(purchaseId),
+                () -> assertThat(returnLottos.get(0).getNumbers()).isEqualTo("1,2,3,4,5,6"),
+                () -> assertThat(returnLottos.get(1).getNumbers()).isEqualTo("7,8,9,10,11,12")
+        );
+    }
 }
