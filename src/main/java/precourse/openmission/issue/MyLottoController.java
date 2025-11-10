fb9b12fdb43f0a78b3a6a5e91d6a7b0994a6551c
seokhan0731 <wistin@naver.com>
2025-11-10 23:55:28 +0900

    feat: 잘못된 구매 Id로 Api 요청 시, 상태 코드 400과 해당 에러 메시지 반환

--- src/main/java/precourse/openmission/issue/MyLottoController.java
+++ src/main/java/precourse/openmission/issue/MyLottoController.java
@@ -1,10 +1,9 @@
 package precourse.openmission.issue;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,5 +31,10 @@ public class MyLottoController {
         return myLottos.stream()
                 .map(MyLottoResponseDTO::new)
                 .toList();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
