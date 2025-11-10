177fc90ffe36b1b856e77cd6f85ab0d0fd7a8459
seokhan0731 <wistin@naver.com>
2025-11-10 23:41:03 +0900

    feat: 발행 로또 조회 GET 요청시, db 필드를 DTO로 묶어 JSON 형식으로 반환

--- src/main/java/precourse/openmission/issue/MyLottoController.java
+++ src/main/java/precourse/openmission/issue/MyLottoController.java
@@ -1,6 +1,7 @@
 package precourse.openmission.issue;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,17 @@ public class MyLottoController {
     @PostMapping(value = "/issue/{id}")
     public List<MyLottoResponseDTO> issue(@PathVariable Long id) {
         List<MyLotto> lottos = myLottoService.saveLottos(id);
+
+        List<MyLottoResponseDTO> responseDTOs = lottos.stream()
+                .map(lotto -> new MyLottoResponseDTO(lotto))
+                .toList();
+
+        return responseDTOs;
+    }
+
+    @GetMapping(value = "/mylotto/{id}")
+    public List<MyLottoResponseDTO> getMyLotto(@PathVariable Long id) {
+        List<MyLotto> lottos = myLottoService.getLottos(id);
 
         List<MyLottoResponseDTO> responseDTOs = lottos.stream()
                 .map(lotto -> new MyLottoResponseDTO(lotto))
