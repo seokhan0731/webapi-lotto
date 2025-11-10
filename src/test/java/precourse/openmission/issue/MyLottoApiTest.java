49cd539aad8f7c0b13cd6e5ba73f8c3d266cb996
seokhan0731 <wistin@naver.com>
2025-11-10 23:26:42 +0900

    test: 로또 발행 POST 요청시, 발행 및 결과값 반환 테스트

---
+++ src/test/java/precourse/openmission/issue/MyLottoApiTest.java
@@ -0,0 +1,79 @@
+package precourse.openmission.issue;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import precourse.openmission.ApiTest;
+import precourse.openmission.domain.NumberGenerator;
+import precourse.openmission.purchase.Purchase;
+import precourse.openmission.purchase.PurchaseRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class MyLottoApiTest extends ApiTest {
+    @Autowired
+    MyLottoRepository myLottoRepository;
+
+    @Autowired
+    PurchaseRepository purchaseRepository;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public NumberGenerator numberGenerator() {
+            return Mockito.mock(NumberGenerator.class);
+        }
+    }
+
+    @Autowired
+    NumberGenerator numberGenerator;
+
+    Purchase savedPurchase;
+    Long purchaseId;
+
+    @BeforeEach
+    void setUp() {
+        //Given
+        myLottoRepository.deleteAll();
+        purchaseRepository.deleteAll();
+
+        Purchase purchase = new Purchase(2000, 2);
+        savedPurchase = purchaseRepository.save(purchase);
+        purchaseId = savedPurchase.getId();
+        Mockito.when(numberGenerator.generateNumbers())
+                .thenReturn(List.of(1, 2, 3, 4, 5, 6))
+                .thenReturn(List.of(7, 8, 9, 10, 11, 12));
+    }
+
+    @DisplayName("발행 API 정상 작동 확인")
+    @Test
+    void issue() {
+        //When
+        ExtractableResponse<Response> response = RestAssured.given()
+                .when()
+                .post("/issue/" + purchaseId)
+                .then()
+                .extract();
+
+        //Then
+        JsonPath jsonPath = response.jsonPath();
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(200),
+                () -> assertThat(response.header("Content-Type")).isEqualTo("application/json"),
+                () -> assertThat(jsonPath.getList("$")).hasSize(2),
+                () -> assertThat(jsonPath.getString("[0].numbers")).isEqualTo("1,2,3,4,5,6"),
+                () -> assertThat(jsonPath.getString("[1].numbers")).isEqualTo("7,8,9,10,11,12")
+        );
+    }
+}
