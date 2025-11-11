7410182e4e2d8595d86aadad74da9f82057f59a2
seokhan0731 <wistin@naver.com>
2025-11-11 21:57:45 +0900

    feat: 당첨 로또 JPA를 통한 저장 및 조회 구현

---
+++ src/main/java/precourse/openmission/winning/WinningLotto.java
@@ -0,0 +1,28 @@
+package precourse.openmission.winning;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import precourse.openmission.purchase.Purchase;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WinningLotto {
+    @Id
+    private Long id;
+
+    private String numbers;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "id")
+    private Purchase purchase;
+
+
+    public WinningLotto(String numbers, Purchase purchase) {
+        this.numbers = numbers;
+        this.purchase = purchase;
+    }
+}
---
+++ src/main/java/precourse/openmission/winning/WinningRepository.java
@@ -0,0 +1,6 @@
+package precourse.openmission.winning;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WinningRepository extends JpaRepository<WinningLotto, Long> {
+}
