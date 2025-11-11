3365872488399a9387903257265f57dd34f96d32
seokhan0731 <wistin@naver.com>
2025-11-12 00:31:22 +0900

    test: api를 통한 당첨 번호 저장 테스트

---
+++ src/test/java/precourse/openmission/winninglotto/WinningApiTest.java
@@ -0,0 +1,62 @@
+package precourse.openmission.winninglotto;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import precourse.openmission.ApiTest;
+import precourse.openmission.purchase.Purchase;
+import precourse.openmission.purchase.PurchaseRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class WinningApiTest extends ApiTest {
+    @Autowired
+    WinningRepository winningRepository;
+
+    @Autowired
+    PurchaseRepository purchaseRepository;
+
+
+    Long purchaseId;
+
+    @BeforeEach
+    void setUp() {
+        winningRepository.deleteAll();
+        purchaseRepository.deleteAll();
+
+        Purchase purchase = new Purchase(2000, 2);
+        Purchase savedPurchase = purchaseRepository.save(purchase);
+        purchaseId = savedPurchase.getId();
+    }
+
+    @DisplayName("당첨 번호 저장 API 정상 작동 확인")
+    @Test
+    void setWinningLotto() {
+        //Given
+        String numbersJson = "{\"numbers\":[1,2,3,4,5,6]}";
+
+        //When
+        ExtractableResponse<Response> response = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(numbersJson)
+                .when()
+                .post("/issue/winninglotto/" + purchaseId)
+                .then()
+                .extract();
+
+        //Then
+        JsonPath jsonPath = response.body().jsonPath();
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(200),
+                () -> assertThat(response.header("Content-Type")).isEqualTo("application/json"),
+                () -> assertThat(jsonPath.getString("numbers")).isEqualTo("1,2,3,4,5,6")
+        );
+    }
+}
