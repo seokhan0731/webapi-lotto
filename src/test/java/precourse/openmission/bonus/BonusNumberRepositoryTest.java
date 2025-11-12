56ffc3f6c3036f09ab5d7133ac50af4a569ec512
seokhan0731 <wistin@naver.com>
2025-11-12 20:43:25 +0900

    test: 보너스 번호 db에 저장

---
+++ src/test/java/precourse/openmission/bonus/BonusNumberRepositoryTest.java
@@ -0,0 +1,38 @@
+package precourse.openmission.bonus;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import precourse.openmission.purchase.Purchase;
+import precourse.openmission.purchase.PurchaseRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class BonusNumberRepositoryTest {
+    @Autowired
+    BonusNumberRepository bonusNumberRepository;
+
+    @Autowired
+    PurchaseRepository purchaseRepository;
+
+    @DisplayName("보너스 번호 저장")
+    @Test
+    void saveBonus() {
+        //Given
+        Purchase purchase = new Purchase(2000, 2);
+        Purchase savedPurchase = purchaseRepository.save(purchase);
+        Long purchaseId = savedPurchase.getId();
+        String bonus = "1";
+        BonusNumber bonusNumber = new BonusNumber(bonus, purchase);
+
+        //When
+        BonusNumber savedBonus = bonusNumberRepository.save(bonusNumber);
+
+        //Given
+        assertThat(savedBonus.getNumber()).isEqualTo("1");
+        assertThat(savedBonus.getId()).isEqualTo(purchaseId);
+
+    }
+}
